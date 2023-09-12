c3fb245a5b3ed70e5856f448b73160e510783b05
Lawrence <lancecraft3@gmail.com>
2023-09-12 14:58:41 -0300

    Hello World LWM[12-09-2023]
    hello world project for cp programming

---
+++ Hello World LWM/Hello World LWM/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World_LWM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wello Horld!");
+            Console.ReadKey();
+            Console.WriteLine("...");
+            Console.ReadKey();
+            Console.WriteLine("I think...");
+            Console.ReadKey();
+        }
+    }
+}
